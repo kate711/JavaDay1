51cb142d227c7ecbc20b8b8243c5949c788bbcc3
kate711 <kate711@aliyun.com>
2020-05-08 14:26:41 +0800

    202005081421

--- src/www/zmcc/com/HelloWorld.java
+++ src/www/zmcc/com/HelloWorld.java
@@ -3,6 +3,7 @@ package www.zmcc.com;
 public class HelloWorld {
     public static void main (String args[]){
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
     }
 }
